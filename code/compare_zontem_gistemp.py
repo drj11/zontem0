76fcbfdb35af5bdc685ecad9ec9a326abb02e165
David Jones <drj@pobox.com>
2014-07-19 21:08:13 +0100

    write out a GHCN-M v3 .dat file as well

--- code/compare_zontem_gistemp.py
+++ code/compare_zontem_gistemp.py
@@ -29,10 +29,34 @@ def join(zontem_dir=os.path.join(HOME, "zontem"),
         zontem_dict = dict((row[0], row) for row in zontem_data)
 
         years = sorted(set(gistemp_dict) & set(zontem_dict))
+        # The ZONTEM annual data.
+        zontem_annual = [zontem_dict[year][1] for year in years]
+        # The GISTEMP annual data.
+        gistemp_annual = [gistemp_dict[year][13] for year in years]
+
         with open("compare_zontem_gistemp.csv", "w") as out:
             csv_out = csv.writer(out)
             csv_out.writerow(["Year", os.path.basename(zontem_csv), os.path.basename(gistemp_csv)])
-            for year in years:
-                csv_out.writerow([year, zontem_dict[year][1], gistemp_dict[year][13]])
+            for year,zontem_v,gistemp_v in zip(
+              years, zontem_annual, gistemp_annual):
+                csv_out.writerow([year, zontem_v, gistemp_v])
+
+        with open("compare_zontem_gistemp.dat", "w") as dat:
+            for id,values in [('zontem_____', zontem_annual),
+              ('ccc-gistemp', gistemp_annual)]:
+                for year, value in zip(years, values):
+                    months = "{}  f".format(convert1(value)) * 12
+                    dat.write("{}{}TAVG{}\n".format(id, year, months))
+
+def convert1(v):
+    """
+    Convert decimal string to 5 character GHCN-M value.
+    """
+
+    try:
+        x = float(v)
+    except ValueError:
+        return "-9999"
+    return "{:5.0f}".format(x*100)
 
 join()
