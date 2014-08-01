a68f87e4e77a32d5174f251669b3e72ee7a5de6a
David Jones <drj@pobox.com>
2014-08-01 10:11:46 +0100

    zone labels

--- code/figure-map.sh
+++ code/figure-map.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 set -e
+
 reticule_path=$(printf '<path d="M 0 %d h 720" />\\\n' $(seq 0 18 360))
+
+zone_text=$(
+  for i in $(seq 0 19)
+  do
+      printf '<text alignment-baseline="middle" font-size="12.0" x="300" y="'$(expr 351 - $i \* 18)'">Zone '"$i"'</text>\\\n' "$i"
+  done
+)
 
 ~/shptosvg/code/tosvg.py --transform=eqarea ~/.local/share/data/ne_110m_land/*.shp |
   sed '$i\
 <g class="reticule">\
 '"$reticule_path"'
+</g>\
+<g class="zone-label">\
+'"$zone_text"'
 </g>
 /^ *<style/a\
 g.reticule { stroke: white; stroke-width: 1.4; }
