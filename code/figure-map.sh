ffe84f45dceef542f6568aee3ea2699a4af87814
David Jones <drj@pobox.com>
2014-08-01 11:06:21 +0100

    show number of stations used in each zone

--- code/figure-map.sh
+++ code/figure-map.sh
@@ -5,9 +5,17 @@ set -e
 reticule_path=$(printf '<path d="M 0 %d h 720" />\\\n' $(seq 0 18 360))
 
 zone_text=$(
+  # Find the number of stations used in each Zone...
+  grep -v 000000000000 ~/zontem/log/zontem.log |
+    grep -n LONGEST |
+    awk -F: 'l{print $1-l};{l=$1}' |
+  # And pipe it into this loop that prints Zone labels.
   for i in $(seq 0 19)
   do
-      printf '<text alignment-baseline="middle" font-size="12.0" x="300" y="'$(expr 351 - $i \* 18)'">Zone '"$i"'</text>\\\n' "$i"
+      y=$(expr 351 - $i \* 18)
+      read n_stations
+      text=$(printf 'Zone %2d %4d stations' "$i" "$n_stations")
+      printf '<text alignment-baseline="middle" font-size="12.0" x="300" y="'$y'">%s</text>\\\n' "$text"
   done
 )
 
