bd9b7acfe500c0b36727532fb3ac8093e026cd60
David Jones <drj@pobox.com>
2014-09-21 11:41:36 +0100

    fiddle with compare to put results in result

--- code/compare_zontem_gistemp.py
+++ code/compare_zontem_gistemp.py
@@ -11,14 +11,15 @@ import re
 
 HOME = os.path.expanduser("~")
 
-def join(zontem_dir=os.path.join(HOME, "zontem"),
-  gistemp_dir=os.path.join(HOME, "ccc-gistemp")):
+def join(zontem=os.path.join(HOME, "zontem"),
+  gistemp=os.path.join(HOME, "ccc-gistemp")):
     import glob
 
     zontem_csv = sorted(glob.glob(os.path.join(
-      zontem_dir, "output", "*ghcnm*.csv")))[-1]
+      zontem, "output", "*ghcnm*.csv")))[-1]
+    # :todo:(drj) glob for landGLB*.
     gistemp_csv = os.path.join(
-      gistemp_dir, "result", "landGLB.Ts.ho2.GHCN.CL.PA.csv")
+      gistemp, "result", "landGLB.Ts.GHCN.CL.PA.csv")
 
     with open(gistemp_csv) as gistemp_f, open(zontem_csv) as zontem_f:
         gistemp = csv.reader(gistemp_f)
@@ -34,14 +35,16 @@ def join(zontem_dir=os.path.join(HOME, "zontem"),
         # The GISTEMP annual data.
         gistemp_annual = [gistemp_dict[year][13] for year in years]
 
-        with open("compare_zontem_gistemp.csv", "w") as out:
+        csv_name = os.path.join("result", "compare_zontem_gistemp.csv")
+        with open(csv_name, "w") as out:
             csv_out = csv.writer(out)
             csv_out.writerow(["Year", os.path.basename(zontem_csv), os.path.basename(gistemp_csv)])
             for year,zontem_v,gistemp_v in zip(
               years, zontem_annual, gistemp_annual):
                 csv_out.writerow([year, zontem_v, gistemp_v])
 
-        with open("compare_zontem_gistemp.dat", "w") as dat:
+        dat_name = os.path.join("result", "compare_zontem_gistemp.dat")
+        with open(dat_name, "w") as dat:
             for id,values in [('zontem_____', zontem_annual),
               ('ccc-gistemp', gistemp_annual)]:
                 for year, value in zip(years, values):
@@ -59,4 +62,19 @@ def convert1(v):
         return "-9999"
     return "{:5.0f}".format(x*100)
 
-join()
+def main(argv=None):
+    import getopt
+    import sys
+
+    if argv is None:
+        argv = sys.argv
+
+    opt, arg = getopt.getopt(argv[1:], '', ['zontem=', 'gistemp='])
+    if arg:
+        return usage()
+
+    k = dict((o.replace('--', ''), v) for o, v in opt)
+    join(**k)
+
+if __name__ == '__main__':
+    main()
