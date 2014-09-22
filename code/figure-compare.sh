c9d0c7a6a625d4c9fd3348c60545041d919e491f
David Jones <drj@pobox.com>
2014-09-22 21:45:04 +0100

    Add CRUTEM4 to figure-compare.svg

--- code/figure-compare.sh
+++ code/figure-compare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/ghcntool/stationplot.py -o result/figure-compare.svg --title "Global Temperature Change" -c ytick=0.2 -c yscale=200 -y -d result/ccc-gistemp.dat ccc-gistemp -d result/zontem.dat zontemglobe
+~/ghcntool/stationplot.py -o result/figure-compare.svg --title "Global Temperature Change" -c ytick=0.2 -c yscale=200 -y -d result/crutem4.dat CRUTEM4GLOB -d result/ccc-gistemp.dat ccc-gistemp -d result/zontem.dat zontemglobe
