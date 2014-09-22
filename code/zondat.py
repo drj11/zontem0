811b4d59d64065804a240ae0f0a721fb1041eb16
David Jones <drj@pobox.com>
2014-09-22 21:57:48 +0100

    Lift IO glob stuff into main() function

--- code/zondat.py
+++ code/zondat.py
@@ -10,28 +10,20 @@ import re
 
 HOME = os.path.expanduser("~")
 
-def to_dat(zontem_dir=os.path.join(HOME, "zontem")):
-    import glob
+def to_dat(zontem_f, out):
+    zontem = csv.reader(zontem_f)
+    zontem_data = [row for row in zontem if re.match(r'[0-9]{4}', row[0])]
 
-    zontem_csv = sorted(glob.glob(os.path.join(
-      zontem_dir, "output", "*ghcnm*.csv")))[-1]
-
-    with open(zontem_csv) as zontem_f:
-        zontem = csv.reader(zontem_f)
-        zontem_data = [row for row in zontem if re.match(r'[0-9]{4}', row[0])]
-        zontem_dict = dict((row[0], row) for row in zontem_data)
-
-        with open(os.path.join("result", "zontem.dat"), "w") as dat:
-            columns = list(zip(*zontem_data))
-            years = columns[0]
-            for i, series in enumerate(columns[1:]):
-                if i == 0:
-                    id = "zontemglobe"
-                else:
-                    id = "zontemzon{:02d}".format(i-1)
-                for year, value in zip(years, series):
-                    months = "{}  f".format(convert1(value)) * 12
-                    dat.write("{}{}TAVG{}\n".format(id, year, months))
+    columns = list(zip(*zontem_data))
+    years = columns[0]
+    for i, series in enumerate(columns[1:]):
+        if i == 0:
+            id = "zontemglobe"
+        else:
+            id = "zontemzon{:02d}".format(i-1)
+        for year, value in zip(years, series):
+            months = "{}  f".format(convert1(value)) * 12
+            out.write("{}{}TAVG{}\n".format(id, year, months))
 
 def convert1(v):
     """
@@ -44,4 +36,20 @@ def convert1(v):
         return "-9999"
     return "{:5.0f}".format(x*100)
 
-to_dat()
+def main(argv=None):
+    import glob
+    import sys
+
+    if argv is None:
+        argv = sys.argv
+
+    zontem_dir = os.path.join(HOME, "zontem")
+    zontem_csv = sorted(glob.glob(os.path.join(
+      zontem_dir, "output", "*ghcnm*.csv")))[-1]
+
+    with open(zontem_csv) as f:
+        with open(os.path.join("result", "zontem.dat"), "w") as dat:
+            to_dat(f, dat)
+
+if __name__ == '__main__':
+    main()
